06a2590e6a37028209628958894fc7598589ec78
Vadim Kaushan <admin@disasm.info>
2013-05-13 15:03:38 +0400

    Models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    owner = models.ForeignKey(User)
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=200, blank=True)
+    filename = models.CharField(max_length=200)
+    uid = models.CharField(max_length=50)
+    owner = models.ForeignKey(User)
+    tags = models.ManyToManyField(Tag, through='ItemTag')
+    created = models.DateTimeField()
+    updated = models.DateTimeField()
+
+class ItemTag(models.Model):
+    item = models.ForeignKey(Item)
+    tag = models.ForeignKey(Tag)
+
