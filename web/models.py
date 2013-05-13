9031817d1fc15074d073f67eae4f3a6b66e2927f
Vadim Kaushan <admin@disasm.info>
2013-05-14 00:42:25 +0400

    Model helpers (todo)

--- web/models.py
+++ web/models.py
@@ -1,10 +1,52 @@
-from django.db import models
+from django.db import models, connection
 from django.contrib.auth.models import User
 from django.conf import settings
+
+class TagManager(models.Manager):
+    def subitems_ids(self, tags):
+        ids = []
+        for tag in tags:
+            ids.append(str(tag.id))
+        n = len(ids)
+        ids = ", ".join(ids)
+
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT item_id FROM web_itemtag
+            WHERE tag_id IN (%s) GROUP BY item_id
+            HAVING count(*) = %d""" % (ids, n))
+        ids = []
+        for row in cursor.fetchall():
+            ids.append(int(row[0]))
+        return ids
+
+    def subitems(self, tags):
+        ids = self.subitems_ids(tags)
+        return self.get_queryset().filter(id__in=ids)
+
+    def subtags_ids(self, tags):
+        ids = self.subitems_ids(tags)
+        ids = ", ".join(ids)
+
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT tag_id, COUNT(*) FROM web_itemtag
+            WHERE item_id IN (%s) GROUP BY tag_id""" % ids)
+        result = {}
+        for row in cursor.fetchall():
+            result[int(row[0])] = int(row[1])
+        return result
+
+    def subtags(self, tags):
+        ids = self.subtags_ids(tags)
+        return self.get_queryset().filter(id__in=ids)
 
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
     owner = models.ForeignKey(User)
+
+    def subtags(self, tags):
+        return []
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
