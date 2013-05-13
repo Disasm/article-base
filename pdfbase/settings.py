5f1a8c1b2ee91f982513f6fa347061faef2e1f47
Vadim Kaushan <admin@disasm.info>
2013-05-13 19:21:25 +0400

    Changed default time format

--- pdfbase/settings.py
+++ pdfbase/settings.py
@@ -43,7 +43,7 @@ USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
-USE_L10N = True
+USE_L10N = False
 
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
@@ -157,3 +157,4 @@ LOGGING = {
 }
 
 ALLOWED_EXTS = ["pdf", "djvu", "djv", "doc", "docx", "ppt", "pptx"]
+DATETIME_FORMAT = "Y-m-d H:i:s"
