30da93fcefed15043d9d4fb41704689b3fd18bf8
Vadim Kaushan <admin@disasm.info>
2015-08-05 07:58:56 +0000

    Admin interface enabled

--- pdfbase/urls.py
+++ pdfbase/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -23,5 +23,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
