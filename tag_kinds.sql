d87b16103be35f1bac925797dd7b7c3869375c33
Vadim Kaushan <admin@disasm.info>
2015-12-04 16:48:18 +0300

    Tag kind SQL script

---
+++ tag_kinds.sql
@@ -0,0 +1,5 @@
+DELETE FROM "web_tagkind";
+INSERT INTO "web_tagkind" VALUES(1,'people','Люди','icon-user');
+INSERT INTO "web_tagkind" VALUES(2,'company','Коллективы','icon-briefcase');
+INSERT INTO "web_tagkind" VALUES(3,'year','Годы издания','icon-calendar');
+INSERT INTO "web_tagkind" VALUES(4,'type','Типы','icon-book');
